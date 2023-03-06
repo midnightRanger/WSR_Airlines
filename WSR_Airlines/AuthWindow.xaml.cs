942d9870c5027b14514ed79c72f7e670d053be41
Lev Ostapenko <012938576a@gmail.com>
2023-03-06 16:38:10 +0300

    added timer

--- WSR_Airlines/AuthWindow.xaml.cs
+++ WSR_Airlines/AuthWindow.xaml.cs
@@ -17,6 +17,7 @@ using System.Data.SqlClient;
 using System.Data;
 using WSR_Airlines.MainSetTableAdapters;
 using WSR_Airlines.Models;
+using System.Windows.Threading;
 
 namespace WSR_Airlines
 {
@@ -27,7 +28,14 @@ namespace WSR_Airlines
     {
         MainSet dataSet;
         UsersTableAdapter usersTableAdapter = new UsersTableAdapter();
-        private bool isLoggedIn = false; 
+        private bool isLoggedIn = false;
+        private Int16 _attempt = 0;
+
+
+        int currentTimerSec = 0; 
+
+        DispatcherTimer dispatcherTimer = new DispatcherTimer();
+        
 
         public AuthWindow()
         {
@@ -43,7 +51,23 @@ namespace WSR_Airlines
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
+            DispatcherTimer timer = new DispatcherTimer();
+        }
 
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            if(currentTimerSec == 10)
+            {
+                dispatcherTimer.Stop();
+                currentTimerSec = 0;
+                loginBTN.Visibility = Visibility.Visible;
+            }
+
+            else 
+                currentTimerSec++;
+
+            timerTB.Text = currentTimerSec.ToString();
+            
         }
 
         private void enterBTN_Click(object sender, RoutedEventArgs e)
@@ -63,15 +87,28 @@ namespace WSR_Airlines
                 }
             }
 
-            if(isLoggedIn)
+            if (isLoggedIn)
             {
                 MessageBox.Show("Данные введены верно");
                 new AdminMenuWindow().Show();
 
                 Close();
             }
-            else 
-            MessageBox.Show("Данные введены неправильно");
+            else
+            {
+                _attempt++;
+                if (_attempt == 3)
+                {
+                    _attempt = 0;
+                    loginBTN.Visibility = Visibility.Hidden;
+
+                    dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+                    dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+                    dispatcherTimer.Start();
+                }
+                MessageBox.Show("Данные введены неправильно");
+                
+            }
             
         }
     }
